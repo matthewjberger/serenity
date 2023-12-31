5a0fc4ebdba54d09ae17943bf1cab09be9a80a1e
Matthew J. Berger <matthewjordanberger@gmail.com>
2023-12-31 07:54:52 -0700

    refactor: refactor into modules

---
+++ src/gpu.rs
@@ -0,0 +1,116 @@
+pub struct Gpu {
+    pub surface: wgpu::Surface,
+    pub device: wgpu::Device,
+    pub queue: wgpu::Queue,
+    pub surface_config: wgpu::SurfaceConfiguration,
+    pub surface_format: wgpu::TextureFormat,
+}
+
+impl Gpu {
+    pub const DEPTH_FORMAT: wgpu::TextureFormat = wgpu::TextureFormat::Depth32Float;
+
+    pub fn new(window: &winit::window::Window, width: u32, height: u32) -> Self {
+        pollster::block_on(Self::new_async(window, width, height))
+    }
+
+    pub fn resize(&mut self, width: u32, height: u32) {
+        log::info!("Resizing renderer surface to: ({width}, {height})");
+        self.surface_config.width = width;
+        self.surface_config.height = height;
+        self.surface.configure(&self.device, &self.surface_config);
+    }
+
+    #[allow(dead_code)]
+    pub fn create_depth_texture(
+        &self,
+        width: u32,
+        height: u32,
+        format: wgpu::TextureFormat,
+    ) -> wgpu::TextureView {
+        let texture = self.device.create_texture(
+            &(wgpu::TextureDescriptor {
+                label: Some("Depth Texture"),
+                size: wgpu::Extent3d {
+                    width,
+                    height,
+                    depth_or_array_layers: 1,
+                },
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format,
+                usage: wgpu::TextureUsages::RENDER_ATTACHMENT
+                    | wgpu::TextureUsages::TEXTURE_BINDING,
+                view_formats: &[],
+            }),
+        );
+        texture.create_view(&wgpu::TextureViewDescriptor::default())
+    }
+
+    async fn new_async(window: &winit::window::Window, width: u32, height: u32) -> Self {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::util::backend_bits_from_env().unwrap_or_else(wgpu::Backends::all),
+            ..Default::default()
+        });
+
+        let surface = unsafe { instance.create_surface(window) }.unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .expect("Failed to request adapter!");
+
+        let required_features = wgpu::Features::empty();
+        let optional_features = wgpu::Features::all();
+
+        let (device, queue) = {
+            log::info!("WGPU Adapter Features: {:#?}", adapter.features());
+            adapter
+                .request_device(
+                    &wgpu::DeviceDescriptor {
+                        features: (optional_features & adapter.features()) | required_features,
+                        // Use the texture resolution limits from the adapter to support images the size of the surface
+                        limits: wgpu::Limits::default().using_resolution(adapter.limits()),
+                        label: Some("Render Device"),
+                    },
+                    None,
+                )
+                .await
+                .expect("Failed to request a device!")
+        };
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+
+        // This assumes an sRGB surface texture
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .find(|f| f.is_srgb())
+            .unwrap_or(surface_capabilities.formats[0]);
+
+        let surface_config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width,
+            height,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+        };
+
+        surface.configure(&device, &surface_config);
+
+        Self {
+            surface,
+            device,
+            queue,
+            surface_config,
+            surface_format,
+        }
+    }
+}
--- src/scene.rs
+++ src/scene.rs
@@ -1,188 +1,15 @@
 use nalgebra_glm as glm;
-use petgraph::Graph;
-use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, fmt::Debug, path::Path};
-use thiserror::Error;
 
-#[derive(Error, Debug)]
-pub enum Error {
-    #[error("Failed to import gltf asset!")]
-    ImportGltfAsset(#[source] gltf::Error),
-
-    #[error("No primitive vertex positions for a primitive in the model.")]
-    ReadVertexPositions,
-}
-
-type Result<T, E = Error> = std::result::Result<T, E>;
-
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+#[derive(Default, Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Scene {
-    pub geometry: Geometry,
     pub graph: SceneGraph,
-}
-
-impl Scene {
-    pub fn import_gltf(&mut self, path: impl AsRef<Path>) -> Result<()> {
-        let (gltf, buffers, _images) =
-            gltf::import(path.as_ref()).map_err(Error::ImportGltfAsset)?;
-        gltf.nodes()
-            .try_for_each(|gltf_node| self.import_node(gltf_node, &buffers))
-    }
-
-    fn import_node(
-        &mut self,
-        gltf_node: gltf::Node<'_>,
-        buffers: &Vec<gltf::buffer::Data>,
-    ) -> Result<(), Error> {
-        let mut node = Node::default();
-        let name = gltf_node.name().unwrap_or("Unnamed");
-        log::info!("Imported node '{name}'...");
-        node.transform = Transform::from(gltf_node.transform().decomposed());
-        if let Some(mesh) = gltf_node.mesh() {
-            self.import_mesh(mesh, buffers, node)?;
-        }
-        Ok(())
-    }
-
-    fn import_mesh(
-        &mut self,
-        mesh: gltf::Mesh<'_>,
-        buffers: &Vec<gltf::buffer::Data>,
-        mut node: Node,
-    ) -> Result<(), Error> {
-        let name = mesh.name().unwrap_or("Unnamed");
-        log::info!("Importing mesh '{name}'...");
-        let mut primitives = Vec::new();
-        mesh.primitives()
-            .try_for_each(|primitive| self.import_primitive(primitive, buffers, &mut primitives))?;
-        let mesh = MeshGeometry {
-            name: name.to_string(),
-            primitives,
-        };
-        let mesh_key = mesh.name.to_string();
-        self.geometry.meshes.insert(mesh_key.to_string(), mesh);
-        node.mesh = Some(mesh_key);
-        Ok(())
-    }
-
-    fn import_primitive(
-        &mut self,
-        gltf_primitive: gltf::Primitive<'_>,
-        buffers: &Vec<gltf::buffer::Data>,
-        primitives: &mut Vec<Primitive>,
-    ) -> Result<(), Error> {
-        let first_index = self.geometry.indices.len();
-        let first_vertex = self.geometry.vertices.len();
-        let number_of_vertices = self.import_primitive_vertices(&gltf_primitive, buffers)?;
-        let number_of_indices = { self.import_primitive_indices(&gltf_primitive, buffers) };
-        let primitive = Primitive {
-            first_index,
-            first_vertex,
-            number_of_indices,
-            number_of_vertices,
-            material_index: gltf_primitive.material().index(),
-        };
-        log::info!("Constructed primitive: {primitive:#?}");
-        primitives.push(primitive);
-        Ok(())
-    }
-
-    fn import_primitive_indices(
-        &mut self,
-        gltf_primitive: &gltf::Primitive<'_>,
-        buffers: &Vec<gltf::buffer::Data>,
-    ) -> usize {
-        let reader = gltf_primitive.reader(|buffer| Some(&*buffers[buffer.index()]));
-        let vertex_count = self.geometry.vertices.len();
-        if let Some(read_indices) = reader.read_indices().take() {
-            let indices = read_indices
-                .into_u32()
-                .map(|x| x + vertex_count as u32)
-                .collect::<Vec<_>>();
-            let number_of_indices = indices.len();
-            self.geometry.indices.extend_from_slice(&indices);
-            number_of_indices
-        } else {
-            0
-        }
-    }
-
-    fn import_primitive_vertices(
-        &mut self,
-        gltf_primitive: &gltf::Primitive<'_>,
-        buffers: &Vec<gltf::buffer::Data>,
-    ) -> Result<usize, Error> {
-        let reader = gltf_primitive.reader(|buffer| Some(&*buffers[buffer.index()]));
-        let mut positions = Vec::new();
-        let read_positions = reader.read_positions().ok_or(Error::ReadVertexPositions)?;
-        for position in read_positions {
-            positions.push(glm::Vec3::from(position));
-        }
-        let number_of_vertices = positions.len();
-        let normals = reader.read_normals().map_or(
-            vec![glm::vec3(0.0, 0.0, 0.0); number_of_vertices],
-            |normals| normals.map(glm::Vec3::from).collect::<Vec<_>>(),
-        );
-        let map_to_vec2 = |coords: gltf::mesh::util::ReadTexCoords<'_>| -> Vec<glm::Vec2> {
-            coords.into_f32().map(glm::Vec2::from).collect::<Vec<_>>()
-        };
-        let uv_0 = reader
-            .read_tex_coords(0)
-            .map_or(vec![glm::vec2(0.0, 0.0); number_of_vertices], map_to_vec2);
-        let uv_1 = reader
-            .read_tex_coords(1)
-            .map_or(vec![glm::vec2(0.0, 0.0); number_of_vertices], map_to_vec2);
-        let convert_joints = |joints: gltf::mesh::util::ReadJoints<'_>| -> Vec<glm::Vec4> {
-            joints
-                .into_u16()
-                .map(|joint| glm::vec4(joint[0] as _, joint[1] as _, joint[2] as _, joint[3] as _))
-                .collect::<Vec<_>>()
-        };
-        let joints_0 = reader.read_joints(0).map_or(
-            vec![glm::vec4(0.0, 0.0, 0.0, 0.0); number_of_vertices],
-            convert_joints,
-        );
-        let convert_weights = |weights: gltf::mesh::util::ReadWeights<'_>| -> Vec<glm::Vec4> {
-            weights.into_f32().map(glm::Vec4::from).collect::<Vec<_>>()
-        };
-        let weights_0 = reader.read_weights(0).map_or(
-            vec![glm::vec4(1.0, 0.0, 0.0, 0.0); number_of_vertices],
-            convert_weights,
-        );
-        let convert_colors = |colors: gltf::mesh::util::ReadColors<'_>| -> Vec<glm::Vec3> {
-            colors
-                .into_rgb_f32()
-                .map(glm::Vec3::from)
-                .collect::<Vec<_>>()
-        };
-        let colors_0 = reader.read_colors(0).map_or(
-            vec![glm::vec3(1.0, 1.0, 1.0); number_of_vertices],
-            convert_colors,
-        );
-        for (index, position) in positions.into_iter().enumerate() {
-            self.geometry.vertices.push(Vertex {
-                position,
-                normal: normals[index],
-                uv_0: uv_0[index],
-                uv_1: uv_1[index],
-                joint_0: joints_0[index],
-                weight_0: weights_0[index],
-                color_0: colors_0[index],
-            });
-        }
-        Ok(number_of_vertices)
-    }
-}
-
-#[derive(Default, Debug, Serialize, Deserialize, Clone)]
-pub struct Geometry {
-    pub vertices: Vec<Vertex>,
-    pub indices: Vec<u32>,
-    pub meshes: HashMap<Mesh, MeshGeometry>,
+    pub meshes: std::collections::HashMap<String, Mesh>,
 }
 
 #[repr(C)]
-#[derive(Debug, Copy, Clone, Serialize, Deserialize, bytemuck::Pod, bytemuck::Zeroable)]
+#[derive(
+    Debug, Copy, Clone, serde::Serialize, serde::Deserialize, bytemuck::Pod, bytemuck::Zeroable,
+)]
 pub struct Vertex {
     pub position: glm::Vec3,
     pub normal: glm::Vec3,
@@ -207,38 +34,67 @@ impl Default for Vertex {
     }
 }
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
-pub struct SceneGraph(pub Graph<Node, ()>);
+#[derive(Default, Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct SceneGraph(pub petgraph::Graph<Node, ()>);
 
-pub type Mesh = String;
+impl SceneGraph {
+    pub fn as_dotviz(&self) -> String {
+        format!(
+            "{:?}",
+            petgraph::dot::Dot::with_config(&self.0, &[petgraph::dot::Config::EdgeNoLabel])
+        )
+    }
+}
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
-pub struct MeshGeometry {
-    pub name: String,
+impl std::ops::Deref for SceneGraph {
+    type Target = petgraph::Graph<Node, ()>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl std::ops::DerefMut for SceneGraph {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[derive(Default, Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct Mesh {
+    pub id: String,
     pub primitives: Vec<Primitive>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Default, Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Primitive {
-    pub first_vertex: usize,
-    pub first_index: usize,
-    pub number_of_vertices: usize,
-    pub number_of_indices: usize,
-    pub material_index: Option<usize>,
+    pub vertices: Vec<Vertex>,
+    pub indices: Vec<u32>,
 }
 
-#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+#[derive(Default, Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub struct Node {
-    name: String,
-    transform: Transform,
-    mesh: Option<Mesh>,
+    pub id: String,
+    pub transform: Transform,
+    pub camera: Option<Camera>,
+    pub mesh: Option<String>,
 }
 
-#[derive(Default, Copy, Clone, Debug, Serialize, Deserialize)]
+#[derive(Copy, Clone, Debug, serde::Serialize, serde::Deserialize)]
 pub struct Transform {
     pub translation: glm::Vec3,
     pub rotation: glm::Quat,
     pub scale: glm::Vec3,
+}
+
+impl Default for Transform {
+    fn default() -> Self {
+        Self {
+            translation: glm::Vec3::new(0.0, 0.0, 0.0),
+            rotation: glm::Quat::identity(),
+            scale: glm::Vec3::new(1.0, 1.0, 1.0),
+        }
+    }
 }
 
 impl From<([f32; 3], [f32; 4], [f32; 3])> for Transform {
@@ -256,5 +112,93 @@ impl From<Transform> for glm::Mat4 {
         glm::translation(&transform.translation)
             * glm::quat_to_mat4(&transform.rotation)
             * glm::scaling(&transform.scale)
+    }
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+pub struct Camera {
+    pub id: String,
+    pub projection: Projection,
+    pub enabled: bool,
+}
+
+impl Camera {
+    #[allow(dead_code)]
+    pub fn projection_matrix(&self, viewport_aspect_ratio: f32) -> glm::Mat4 {
+        match &self.projection {
+            Projection::Perspective(camera) => camera.matrix(viewport_aspect_ratio),
+            Projection::Orthographic(camera) => camera.matrix(),
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn is_orthographic(&self) -> bool {
+        match self.projection {
+            Projection::Perspective(_) => false,
+            Projection::Orthographic(_) => true,
+        }
+    }
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+pub enum Projection {
+    Perspective(PerspectiveCamera),
+    Orthographic(OrthographicCamera),
+}
+
+#[derive(Default, Debug, serde::Serialize, serde::Deserialize, Clone)]
+pub struct PerspectiveCamera {
+    pub aspect_ratio: Option<f32>,
+    pub y_fov_rad: f32,
+    pub z_far: Option<f32>,
+    pub z_near: f32,
+}
+
+impl PerspectiveCamera {
+    pub fn matrix(&self, viewport_aspect_ratio: f32) -> glm::Mat4 {
+        let aspect_ratio = if let Some(aspect_ratio) = self.aspect_ratio {
+            aspect_ratio
+        } else {
+            viewport_aspect_ratio
+        };
+
+        if let Some(z_far) = self.z_far {
+            glm::perspective_zo(aspect_ratio, self.y_fov_rad, self.z_near, z_far)
+        } else {
+            glm::infinite_perspective_rh_zo(aspect_ratio, self.y_fov_rad, self.z_near)
+        }
+    }
+}
+
+#[derive(Default, Debug, serde::Serialize, serde::Deserialize, Clone)]
+pub struct OrthographicCamera {
+    pub x_mag: f32,
+    pub y_mag: f32,
+    pub z_far: f32,
+    pub z_near: f32,
+}
+
+impl OrthographicCamera {
+    pub fn matrix(&self) -> glm::Mat4 {
+        let z_sum = self.z_near + self.z_far;
+        let z_diff = self.z_near - self.z_far;
+        glm::Mat4::new(
+            1.0 / self.x_mag,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.0 / self.y_mag,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            2.0 / z_diff,
+            0.0,
+            0.0,
+            0.0,
+            z_sum / z_diff,
+            1.0,
+        )
     }
 }
