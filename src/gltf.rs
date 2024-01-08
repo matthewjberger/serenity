006c6fc74e87058be5582afaeaaa00e8ba42b493
Matthew J. Berger <matthewjordanberger@gmail.com>
2024-01-08 03:30:24 -0700

    refactor: restructure gltf importing

--- src/gltf.rs
+++ src/gltf.rs
@@ -1,82 +1,33 @@
 pub fn import_gltf(path: impl AsRef<std::path::Path>) -> crate::scene::Scene {
     let (gltf, buffers, raw_images) = gltf::import(path.as_ref()).expect("Failed to import gltf");
+    let (samplers, sampler_ids) = import_samplers(&gltf);
+    let (images, image_ids) = import_images(&raw_images);
+    let (textures, texture_ids) = import_textures(&gltf, sampler_ids, image_ids);
+    let (materials, material_ids) = import_materials(&gltf, texture_ids);
+    let (meshes, mesh_ids) = import_meshes(&gltf, &buffers, material_ids);
+    let (node_ids, graph) = import_graph(&gltf, &mesh_ids);
+    let (animations, _animation_ids) = import_animations(&gltf, &node_ids, &buffers);
+    let (skins, _skin_ids) = import_skins(&gltf, &buffers, &node_ids);
+    crate::scene::Scene {
+        graph,
+        images,
+        samplers,
+        textures,
+        materials,
+        meshes,
+        animations,
+        skins,
+    }
+}
 
-    let mut samplers = std::collections::HashMap::new();
-    samplers.insert("default".to_string(), crate::scene::Sampler::default());
-    let sampler_handles = gltf
-        .samplers()
-        .map(crate::scene::Sampler::from)
-        .map(|sampler| {
-            let id = uuid::Uuid::new_v4().to_string();
-            samplers.insert(id.to_string(), sampler);
-            id
-        })
+fn import_graph(
+    gltf: &gltf::Document,
+    mesh_ids: &[String],
+) -> (Vec<String>, crate::scene::SceneGraph) {
+    let node_ids = gltf
+        .nodes()
+        .map(|_| uuid::Uuid::new_v4().to_string())
         .collect::<Vec<_>>();
-
-    let mut images = std::collections::HashMap::new();
-    let image_handles = raw_images
-        .into_iter()
-        .map(crate::scene::Image::from)
-        .map(|image| {
-            let id = uuid::Uuid::new_v4().to_string();
-            images.insert(id.to_string(), image);
-            id
-        })
-        .collect::<Vec<_>>();
-
-    let mut textures = std::collections::HashMap::new();
-    let texture_handles = gltf
-        .textures()
-        .map(|texture| {
-            let id = uuid::Uuid::new_v4().to_string();
-            let sampler = match texture.sampler().index() {
-                Some(index) => sampler_handles[index].to_string(),
-                None => "default".to_string(),
-            };
-            textures.insert(
-                id.to_string(),
-                crate::scene::Texture {
-                    label: texture.name().unwrap_or("Unnamed texture").to_string(),
-                    image: image_handles[texture.source().index()].to_string(),
-                    sampler,
-                },
-            );
-            id
-        })
-        .collect::<Vec<_>>();
-
-    let mut materials = std::collections::HashMap::new();
-    materials.insert("default".to_string(), crate::scene::Material::default());
-    let material_handles = gltf
-        .materials()
-        .map(|primitive_material| {
-            let pbr = primitive_material.pbr_metallic_roughness();
-            let id = uuid::Uuid::new_v4().to_string();
-            let mut material = crate::scene::Material {
-                base_color_factor: nalgebra_glm::Vec4::from(pbr.base_color_factor()),
-                ..Default::default()
-            };
-            if let Some(base_color_texture) = pbr.base_color_texture() {
-                material.base_color_texture =
-                    texture_handles[base_color_texture.texture().index()].to_string();
-            }
-            materials.insert(id.to_string(), material);
-            id
-        })
-        .collect::<Vec<_>>();
-
-    let mut meshes = std::collections::HashMap::new();
-    meshes.insert("default".to_string(), crate::scene::Mesh::default());
-    let mesh_handles = gltf
-        .meshes()
-        .map(|primitive_mesh| {
-            let id = uuid::Uuid::new_v4().to_string();
-            let mesh = import_mesh(primitive_mesh, &buffers, &material_handles);
-            meshes.insert(id.to_string(), mesh);
-            id
-        })
-        .collect::<Vec<_>>();
-
     let mut first_scenegraph = None;
     let mut scenegraphs = std::collections::HashMap::new();
     scenegraphs.insert("Main".to_string(), crate::scene::SceneGraph::default());
@@ -91,20 +42,256 @@ pub fn import_gltf(path: impl AsRef<std::path::Path>) -> crate::scene::Scene {
             ..Default::default()
         });
         gltf_scene.nodes().for_each(|node| {
-            import_node(root_node, node, &mut graph, &mesh_handles);
+            import_node(root_node, node, &mut graph, mesh_ids, &node_ids);
         });
         scenegraphs.insert(id.to_string(), graph);
     });
     let graph = scenegraphs[&first_scenegraph.unwrap_or("Main".to_string())].clone();
+    (node_ids, graph)
+}
 
-    crate::scene::Scene {
-        graph,
-        images,
-        samplers,
-        textures,
-        materials,
-        meshes,
-    }
+fn import_samplers(
+    gltf: &gltf::Document,
+) -> (
+    std::collections::HashMap<String, crate::scene::Sampler>,
+    Vec<String>,
+) {
+    let mut samplers = std::collections::HashMap::new();
+    samplers.insert("default".to_string(), crate::scene::Sampler::default());
+    let sampler_ids = gltf
+        .samplers()
+        .map(crate::scene::Sampler::from)
+        .map(|sampler| {
+            let id = uuid::Uuid::new_v4().to_string();
+            samplers.insert(id.to_string(), sampler);
+            id
+        })
+        .collect::<Vec<_>>();
+    (samplers, sampler_ids)
+}
+
+fn import_images(
+    raw_images: &[gltf::image::Data],
+) -> (
+    std::collections::HashMap<String, crate::scene::Image>,
+    Vec<String>,
+) {
+    let mut images = std::collections::HashMap::new();
+    let image_ids = raw_images
+        .iter()
+        .cloned()
+        .map(crate::scene::Image::from)
+        .map(|image| {
+            let id = uuid::Uuid::new_v4().to_string();
+            images.insert(id.to_string(), image);
+            id
+        })
+        .collect::<Vec<_>>();
+    (images, image_ids)
+}
+
+fn import_textures(
+    gltf: &gltf::Document,
+    sampler_ids: Vec<String>,
+    image_ids: Vec<String>,
+) -> (
+    std::collections::HashMap<String, crate::scene::Texture>,
+    Vec<String>,
+) {
+    let mut textures = std::collections::HashMap::new();
+    let texture_ids = gltf
+        .textures()
+        .map(|texture| {
+            let id = uuid::Uuid::new_v4().to_string();
+            let sampler = match texture.sampler().index() {
+                Some(index) => sampler_ids[index].to_string(),
+                None => "default".to_string(),
+            };
+            textures.insert(
+                id.to_string(),
+                crate::scene::Texture {
+                    label: texture.name().unwrap_or("Unnamed texture").to_string(),
+                    image: image_ids[texture.source().index()].to_string(),
+                    sampler,
+                },
+            );
+            id
+        })
+        .collect::<Vec<_>>();
+    (textures, texture_ids)
+}
+
+fn import_materials(
+    gltf: &gltf::Document,
+    texture_ids: Vec<String>,
+) -> (
+    std::collections::HashMap<String, crate::scene::Material>,
+    Vec<String>,
+) {
+    let mut materials = std::collections::HashMap::new();
+    materials.insert("default".to_string(), crate::scene::Material::default());
+    let material_ids = gltf
+        .materials()
+        .map(|primitive_material| {
+            let pbr = primitive_material.pbr_metallic_roughness();
+            let id = uuid::Uuid::new_v4().to_string();
+            let mut material = crate::scene::Material {
+                base_color_factor: nalgebra_glm::Vec4::from(pbr.base_color_factor()),
+                ..Default::default()
+            };
+            if let Some(base_color_texture) = pbr.base_color_texture() {
+                material.base_color_texture =
+                    texture_ids[base_color_texture.texture().index()].to_string();
+            }
+            materials.insert(id.to_string(), material);
+            id
+        })
+        .collect::<Vec<_>>();
+    (materials, material_ids)
+}
+
+fn import_meshes(
+    gltf: &gltf::Document,
+    buffers: &[gltf::buffer::Data],
+    material_ids: Vec<String>,
+) -> (
+    std::collections::HashMap<String, crate::scene::Mesh>,
+    Vec<String>,
+) {
+    let mut meshes = std::collections::HashMap::new();
+    meshes.insert("default".to_string(), crate::scene::Mesh::default());
+    let mesh_ids = gltf
+        .meshes()
+        .map(|primitive_mesh| {
+            let id = uuid::Uuid::new_v4().to_string();
+            let mesh = import_mesh(primitive_mesh, buffers, &material_ids);
+            meshes.insert(id.to_string(), mesh);
+            id
+        })
+        .collect::<Vec<_>>();
+    (meshes, mesh_ids)
+}
+
+fn import_skins(
+    gltf: &gltf::Document,
+    buffers: &[gltf::buffer::Data],
+    node_ids: &[String],
+) -> (
+    std::collections::HashMap<String, crate::scene::Skin>,
+    Vec<String>,
+) {
+    let mut skins = std::collections::HashMap::new();
+    let skin_ids = gltf
+        .skins()
+        .map(|gltf_skin| {
+            let id = uuid::Uuid::new_v4().to_string();
+            let reader = gltf_skin.reader(|buffer| Some(&buffers[buffer.index()]));
+            let inverse_bind_matrices = reader
+                .read_inverse_bind_matrices()
+                .map_or(Vec::new(), |matrices| {
+                    matrices.map(nalgebra_glm::Mat4::from).collect::<Vec<_>>()
+                });
+            let joints = gltf_skin
+                .joints()
+                .enumerate()
+                .map(|(index, joint_node)| {
+                    let inverse_bind_matrix = *inverse_bind_matrices
+                        .get(index)
+                        .unwrap_or(&nalgebra_glm::Mat4::identity());
+                    crate::scene::Joint {
+                        inverse_bind_matrix,
+                        target: node_ids[joint_node.index()].to_string(),
+                    }
+                })
+                .collect();
+            let label = gltf_skin.name().unwrap_or("Unnamed Skin").to_string();
+            skins.insert(id.to_string(), crate::scene::Skin { label, joints });
+            id
+        })
+        .collect::<Vec<_>>();
+    (skins, skin_ids)
+}
+
+fn import_animations(
+    gltf: &gltf::Document,
+    node_ids: &[String],
+    buffers: &[gltf::buffer::Data],
+) -> (
+    std::collections::HashMap<String, crate::scene::Animation>,
+    Vec<String>,
+) {
+    let mut animations = std::collections::HashMap::new();
+    let ids = gltf
+        .animations()
+        .map(|gltf_animation| {
+            let id = uuid::Uuid::new_v4().to_string();
+            let channels = gltf_animation
+                .channels()
+                .map(|channel| {
+                    let target_node = channel.target().node().index();
+                    let target = node_ids[target_node].to_string();
+                    let reader = channel.reader(|buffer| Some(&buffers[buffer.index()]));
+                    let inputs = reader
+                        .read_inputs()
+                        .expect("Failed to read animation channel inputs!")
+                        .collect::<Vec<_>>();
+                    let outputs = reader
+                        .read_outputs()
+                        .expect("Failed to read animation channel outputs!");
+                    let transformations = match outputs {
+                        gltf::animation::util::ReadOutputs::Translations(translations) => {
+                            let translations = translations
+                                .map(nalgebra_glm::Vec3::from)
+                                .collect::<Vec<_>>();
+                            crate::scene::TransformationSet::Translations(translations)
+                        }
+                        gltf::animation::util::ReadOutputs::Rotations(rotations) => {
+                            let rotations = rotations
+                                .into_f32()
+                                .map(nalgebra_glm::Vec4::from)
+                                .collect::<Vec<_>>();
+                            crate::scene::TransformationSet::Rotations(rotations)
+                        }
+                        gltf::animation::util::ReadOutputs::Scales(scales) => {
+                            let scales = scales.map(nalgebra_glm::Vec3::from).collect::<Vec<_>>();
+                            crate::scene::TransformationSet::Scales(scales)
+                        }
+                        gltf::animation::util::ReadOutputs::MorphTargetWeights(weights) => {
+                            let morph_target_weights = weights.into_f32().collect::<Vec<_>>();
+                            crate::scene::TransformationSet::MorphTargetWeights(
+                                morph_target_weights,
+                            )
+                        }
+                    };
+                    crate::scene::Channel {
+                        target,
+                        inputs,
+                        transformations,
+                        interpolation: crate::scene::Interpolation::default(),
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            let max_animation_time = channels
+                .iter()
+                .flat_map(|channel| channel.inputs.iter().copied())
+                .fold(0.0, f32::max);
+            animations.insert(
+                id.to_string(),
+                crate::scene::Animation {
+                    label: gltf_animation
+                        .name()
+                        .unwrap_or("Unnamed animation")
+                        .to_string(),
+                    channels,
+                    time: 0.0,
+                    max_animation_time,
+                },
+            );
+            id
+        })
+        .collect::<Vec<_>>();
+    (animations, ids)
 }
 
 impl From<gltf::material::AlphaMode> for crate::scene::AlphaMode {
@@ -197,33 +384,29 @@ fn import_node(
     gltf_node: gltf::Node,
     scenegraph: &mut crate::scene::SceneGraph,
     mesh_handles: &[String],
+    node_ids: &[String],
 ) {
-    let name = gltf_node.name().unwrap_or("Unnamed node");
-
-    let mut scene_node = crate::scene::Node {
-        label: name.to_string(),
-        transform: crate::scene::Transform::from(gltf_node.transform().decomposed()),
-        ..Default::default()
-    };
+    let mut components = Vec::new();
 
     if let Some(mesh) = gltf_node.mesh() {
         let mesh_id = mesh_handles[mesh.index()].to_string();
-        scene_node
-            .components
-            .push(crate::scene::NodeComponent::Mesh(mesh_id));
+        components.push(crate::scene::NodeComponent::Mesh(mesh_id));
     }
 
     if let Some(camera) = gltf_node.camera() {
-        scene_node
-            .components
-            .push(crate::scene::NodeComponent::Camera(camera.into()));
+        components.push(crate::scene::NodeComponent::Camera(camera.into()));
     }
 
     if let Some(light) = gltf_node.light() {
-        scene_node
-            .components
-            .push(crate::scene::NodeComponent::Light(light.into()));
+        components.push(crate::scene::NodeComponent::Light(light.into()));
     }
+
+    let scene_node = crate::scene::Node {
+        id: node_ids[gltf_node.index()].to_string().to_string(),
+        label: gltf_node.name().unwrap_or("Unnamed node").to_string(),
+        transform: crate::scene::Transform::from(gltf_node.transform().decomposed()),
+        components,
+    };
 
     let node_index = scenegraph.add_node(scene_node);
 
@@ -232,7 +415,7 @@ fn import_node(
     }
 
     gltf_node.children().for_each(|child| {
-        import_node(node_index, child, scenegraph, mesh_handles);
+        import_node(node_index, child, scenegraph, mesh_handles, node_ids);
     });
 }
 
