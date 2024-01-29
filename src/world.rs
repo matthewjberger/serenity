ad71d4fa8dea938c114726385202f71b34731a9a
Matthew J. Berger <matthewjordanberger@gmail.com>
2024-01-29 01:37:39 -0700

    chore: correct global transform

--- src/world.rs
+++ src/world.rs
@@ -19,117 +19,15 @@ pub struct World {
 }
 
 impl World {
-    pub fn merge_world(&mut self, world: &World) {
-        let camera_offset = self.cameras.len();
-        let image_offset = self.images.len();
-        let index_offset = self.indices.len();
-        let light_offset = self.lights.len();
-        let material_offset = self.materials.len();
-        let mesh_offset = self.meshes.len();
-        let metadata_offset = self.metadata.len();
-        let node_offset = self.nodes.len();
-        let sampler_offset = self.samplers.len();
-        let texture_offset = self.textures.len();
-        let transform_offset = self.transforms.len();
-        let vertex_offset = self.vertices.len();
-
-        world.animations.iter().cloned().for_each(|animation| {
-            let mut animation = animation.clone();
-            animation.channels.iter_mut().for_each(|channel| {
-                channel.target_node_index += node_offset;
-            });
-            self.animations.push(animation);
-        });
-
-        world.cameras.iter().cloned().for_each(|camera| {
-            self.cameras.push(camera);
-        });
-
-        world.images.iter().cloned().for_each(|image| {
-            self.images.push(image);
-        });
-
-        world
-            .indices
-            .iter()
-            .cloned()
-            .for_each(|index| self.indices.push(index + vertex_offset as u32));
-
-        world.lights.iter().cloned().for_each(|light| {
-            self.lights.push(light);
-        });
-
-        world.materials.iter().cloned().for_each(|material| {
-            let mut material = material.clone();
-            material.base_color_texture_index = material.base_color_texture_index + texture_offset;
-            material.emissive_texture_index = material.emissive_texture_index + texture_offset;
-            self.materials.push(material);
-        });
-
-        world.meshes.iter().for_each(|mesh| {
-            let mut mesh = mesh.clone();
-            mesh.primitives.iter_mut().for_each(|primitive| {
-                primitive.vertex_offset += vertex_offset;
-                primitive.index_offset += index_offset;
-                primitive.material_index = primitive.material_index.map(|i| i + material_offset);
-            });
-            self.meshes.push(mesh);
-        });
-
-        world.nodes.iter().cloned().for_each(|node| {
-            let mut node = node.clone();
-            node.transform_index += transform_offset;
-            node.metadata_index += metadata_offset;
-            node.mesh_index = node.mesh_index.map(|i| i + mesh_offset);
-            node.camera_index = node.camera_index.map(|i| i + camera_offset);
-            node.light_index = node.light_index.map(|i| i + light_offset);
-            self.nodes.push(node);
-        });
-
-        world.metadata.iter().cloned().for_each(|metadata| {
-            self.metadata.push(metadata);
-        });
-
-        world.samplers.iter().cloned().for_each(|sampler| {
-            self.samplers.push(sampler);
-        });
-
-        world.scenes.iter().cloned().for_each(|mut scene| {
-            scene.graph.node_indices().for_each(|graph_node_index| {
-                scene.graph[graph_node_index] += node_offset;
-            });
-            self.scenes.push(scene);
-        });
-
-        world.skins.iter().cloned().for_each(|skin| {
-            let mut skin = skin.clone();
-            skin.joints.iter_mut().for_each(|joint| {
-                joint.target_node_index += node_offset;
-            });
-            self.skins.push(skin);
-        });
-
-        world.textures.iter().for_each(|texture| {
-            let mut texture = texture.clone();
-            texture.image_index += image_offset;
-            texture.sampler_index = texture.sampler_index.map(|i| i + sampler_offset);
-            self.textures.push(texture);
-        });
-
-        world.transforms.iter().cloned().for_each(|transform| {
-            self.transforms.push(transform);
-        });
-
-        world.vertices.iter().cloned().for_each(|vertex| {
-            self.vertices.push(vertex);
-        });
-    }
-
     pub fn global_transform(
         &self,
         scenegraph: &SceneGraph,
         graph_node_index: petgraph::graph::NodeIndex,
     ) -> nalgebra_glm::Mat4 {
+        if graph_node_index == petgraph::graph::NodeIndex::default() {
+            return nalgebra_glm::Mat4::identity();
+        }
+
         let node_index = scenegraph[graph_node_index];
         let transform_index = self.nodes[node_index].transform_index;
         let transform = self.transforms[transform_index].matrix();
