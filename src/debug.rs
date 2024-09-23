a918a6d5e9cf2d32e4798082fb33aa3628471591
Matthew J. Berger <matthewjordanberger@gmail.com>
2024-09-22 21:37:52 -0600

    add debug grid

--- src/debug.rs
+++ src/debug.rs
@@ -1,5 +1,7 @@
 // TODO: add other shapes for instanced debug display
 //       line, sphere, capsule
+use nalgebra_glm as glm;
+use wgpu::util::DeviceExt;
 
 pub struct DebugRender {
     pub vertex_buffer: wgpu::Buffer,
@@ -8,6 +10,9 @@ pub struct DebugRender {
     pub uniform_buffer: wgpu::Buffer,
     pub uniform_bind_group: wgpu::BindGroup,
     pub cube_pipeline: wgpu::RenderPipeline,
+    pub line_pipeline: wgpu::RenderPipeline,
+    pub grid_vertices: wgpu::Buffer,
+    pub grid_instances: wgpu::Buffer,
 }
 
 impl DebugRender {
@@ -74,6 +79,10 @@ impl DebugRender {
 
         let cube_pipeline = create_cube_pipeline(gpu, &uniform_bind_group_layout);
 
+        let line_pipeline = create_line_pipeline(gpu, &uniform_bind_group_layout);
+
+        let (grid_vertices, grid_instances) = create_grid_buffers(gpu);
+
         Self {
             vertex_buffer,
             index_buffer,
@@ -81,6 +90,9 @@ impl DebugRender {
             uniform_buffer,
             uniform_bind_group,
             cube_pipeline,
+            line_pipeline,
+            grid_vertices,
+            grid_instances,
         }
     }
 
@@ -107,10 +119,18 @@ impl DebugRender {
             );
 
             render_pass.set_bind_group(0, &self.uniform_bind_group, &[]);
+
+            // Render grid
+            render_pass.set_pipeline(&self.line_pipeline);
+            render_pass.set_vertex_buffer(0, self.grid_vertices.slice(..));
+            render_pass.set_vertex_buffer(1, self.grid_instances.slice(..));
+            let num_grid_lines = (GRID_SIZE + 1) * 2; // Vertical + Horizontal lines
+            render_pass.draw(0..2, 0..num_grid_lines);
+
+            // Render debug shapes
+            render_pass.set_pipeline(&self.cube_pipeline);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             render_pass.set_vertex_buffer(1, self.instance_buffer.slice(..));
-
-            render_pass.set_pipeline(&self.cube_pipeline);
             render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
 
             [crate::world::Shape::Cube, crate::world::Shape::CubeExtents]
@@ -402,3 +422,203 @@ fn fragment_main(in: VertexOutput) -> @location(0) vec4<f32> {
     return in.color;
 }
 ";
+
+fn create_line_pipeline(
+    gpu: &crate::gpu::Gpu,
+    uniform_bind_group_layout: &wgpu::BindGroupLayout,
+) -> wgpu::RenderPipeline {
+    let shader_module = gpu
+        .device
+        .create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: None,
+            source: wgpu::ShaderSource::Wgsl(std::borrow::Cow::Borrowed(LINE_SHADER_SOURCE)),
+        });
+    let pipeline_layout = gpu
+        .device
+        .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: None,
+            bind_group_layouts: &[uniform_bind_group_layout],
+            push_constant_ranges: &[],
+        });
+    gpu.device
+        .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader_module,
+                entry_point: "vertex_main",
+                buffers: &[
+                    LineVertex::description(&LineVertex::vertex_attributes()),
+                    LineInstance::description(&LineInstance::vertex_attributes()),
+                ],
+            },
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::LineList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: None,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                conservative: false,
+                unclipped_depth: false,
+            },
+            depth_stencil: Some(wgpu::DepthStencilState {
+                format: wgpu::TextureFormat::Depth32Float,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader_module,
+                entry_point: "fragment_main",
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: gpu.surface_format,
+                    blend: Some(wgpu::BlendState::ALPHA_BLENDING),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            multiview: None,
+        })
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct LineVertex {
+    position: glm::Vec3,
+}
+
+impl LineVertex {
+    pub fn vertex_attributes() -> Vec<wgpu::VertexAttribute> {
+        wgpu::vertex_attr_array![0 => Float32x3].to_vec()
+    }
+
+    pub fn description(attributes: &[wgpu::VertexAttribute]) -> wgpu::VertexBufferLayout {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct LineInstance {
+    start: glm::Vec3,
+    end: glm::Vec3,
+    color: glm::Vec4,
+}
+
+impl LineInstance {
+    pub fn vertex_attributes() -> Vec<wgpu::VertexAttribute> {
+        wgpu::vertex_attr_array![1 => Float32x3, 2 => Float32x3, 3 => Float32x4].to_vec()
+    }
+
+    pub fn description(attributes: &[wgpu::VertexAttribute]) -> wgpu::VertexBufferLayout {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Self>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Instance,
+            attributes,
+        }
+    }
+}
+
+const GRID_SIZE: u32 = 1000;
+const GRID_STEP: f32 = 1.0;
+
+fn create_grid_buffers(gpu: &crate::gpu::Gpu) -> (wgpu::Buffer, wgpu::Buffer) {
+    let vertices = [
+        LineVertex {
+            position: glm::vec3(0.0, 0.0, 0.0),
+        },
+        LineVertex {
+            position: glm::vec3(1.0, 0.0, 0.0),
+        },
+    ];
+
+    let vertex_buffer = gpu
+        .device
+        .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Grid Vertex Buffer"),
+            contents: bytemuck::cast_slice(&vertices),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+    let mut instances = Vec::new();
+    let grid_color = glm::vec4(0.5, 0.5, 0.5, 1.0);
+    let half_size = (GRID_SIZE as f32 * GRID_STEP) / 2.0;
+
+    for i in 0..=GRID_SIZE {
+        let pos = i as f32 * GRID_STEP - half_size;
+        instances.push(LineInstance {
+            start: glm::vec3(pos, 0.0, -half_size),
+            end: glm::vec3(pos, 0.0, half_size),
+            color: grid_color,
+        });
+        instances.push(LineInstance {
+            start: glm::vec3(-half_size, 0.0, pos),
+            end: glm::vec3(half_size, 0.0, pos),
+            color: grid_color,
+        });
+    }
+
+    let instance_buffer = gpu
+        .device
+        .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Grid Instance Buffer"),
+            contents: bytemuck::cast_slice(&instances),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+    (vertex_buffer, instance_buffer)
+}
+
+const LINE_SHADER_SOURCE: &str = r#"
+struct Uniform {
+    view: mat4x4<f32>,
+    projection: mat4x4<f32>,
+    camera_position: vec4<f32>,
+};
+
+@group(0) @binding(0)
+var<uniform> ubo: Uniform;
+
+struct VertexInput {
+    @location(0) position: vec3<f32>,
+};
+
+struct InstanceInput {
+    @location(1) start: vec3<f32>,
+    @location(2) end: vec3<f32>,
+    @location(3) color: vec4<f32>,
+};
+
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) color: vec4<f32>,
+};
+
+@vertex
+fn vertex_main(
+    vert: VertexInput,
+    instance: InstanceInput,
+) -> VertexOutput {
+    let t = vert.position.x;
+    let world_position = mix(instance.start, instance.end, t);
+
+    var out: VertexOutput;
+    out.clip_position = ubo.projection * ubo.view * vec4<f32>(world_position, 1.0);
+    out.color = instance.color;
+    return out;
+}
+
+@fragment
+fn fragment_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    return in.color;
+}
+"#;
